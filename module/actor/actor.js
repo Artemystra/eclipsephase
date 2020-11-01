62537a9a4854016a7a9589c9bf007df8839e584d
Diemenko <diemen@me.com>
2020-11-01 22:31:42 +0100

    Droplists included for Body & Special Skills

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -18,11 +18,26 @@ export class EclipsePhaseActor extends Actor {
     // things organized.
     if (actorData.type === 'character') this._prepareCharacterData(actorData);
 
+    //Physical & Mental derives
       data.physical.wt = Math.floor(data.bodies.morph1.dur / 5);
-      data.physical.dr = data.bodies.morph1.type * data.bodies.morph1.dur;
+      if (data.bodyType.value === 'synth') {
+          data.physical.dr = data.bodies.morph1.dur * 2;
+      }
+      else if (data.bodyType.value === 'bio'){
+          data.physical.dr = data.bodies.morph1.dur * 1.5;
+      }
       data.mental.luc = data.aptitudes.wil.value * 2;
       data.mental.ir = data.mental.luc * 2;
       data.mental.tt = Math.floor(data.mental.luc / 5);
+
+
+    //Special Skill Derives
+      data.specSkills.special1.specCheck = (data.specSkills.special1.value + parseInt(data.specSkills.special1.aptitude, 10));
+      data.specSkills.special2.specCheck = (data.specSkills.special2.value + parseInt(data.specSkills.special2.aptitude, 10));
+      data.specSkills.special3.specCheck = (data.specSkills.special3.value + parseInt(data.specSkills.special3.aptitude, 10));
+      data.specSkills.special4.specCheck = (data.specSkills.special4.value + parseInt(data.specSkills.special4.aptitude, 10));
+      data.specSkills.special5.specCheck = (data.specSkills.special5.value + parseInt(data.specSkills.special5.aptitude, 10));
+      data.specSkills.special6.specCheck = (data.specSkills.special6.value + parseInt(data.specSkills.special6.aptitude, 10));
 
   }
 
@@ -39,6 +54,10 @@ export class EclipsePhaseActor extends Actor {
       if(key === 'program' || key === 'interface' || key === 'infosec' ){
         skill.derived = skill.value + data.aptitudes.cog.value;
         skill.specialized = skill.derived + 10;
+      }
+      else if (key === 'perceive') {
+          skill.derived = skill.value + data.aptitudes.int.value * 2;
+          skill.specialized = skill.derived + 10;
       }
       else {
         skill.derived = skill.value + data.aptitudes.int.value;
@@ -58,6 +77,10 @@ export class EclipsePhaseActor extends Actor {
     for (let [key, skill] of Object.entries(data.skillsVig)) {
       if(key === 'athletics' || key === 'free fall' || key === 'melee' ){
         skill.derived = skill.value + data.aptitudes.som.value;
+        skill.specialized = skill.derived + 10;
+      }
+      else if (key === 'fray'){
+        skill.derived = (skill.value + data.aptitudes.ref.value * 2);
         skill.specialized = skill.derived + 10;
       }
       else {
