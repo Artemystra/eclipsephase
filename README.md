5d72a822b60293c8ad055f5a3c6c4b8d87061b4a
Diemen <diemen@me.com>
2021-04-06 21:02:24 +0200

    This is a System for Eclipse Phase 2nd edition in Foundry VTT. In this version included:
    - 3 different Character types (Actors, NPCs and Goons)
    - Derived stats (Ini, trauma, durability etc.)
    - Better readable chat messages (with success/failure warnings)
    - Drag & droppable (morph)traits, armor, ware & weapons
    - Initiative Tracker + Ini Modifier (on all sheets)
    - Dice roll overlay for skills & damage rolls
    - Vehicles
    
    READY FOR FOUNDRY 0.7.9
    
    planned:
    - Include Muse
    - Armor calculator
    - Advanced PSI checks
    - Multiple ID/Body support
    
    In this update:
    - Added a new "Guns-Skill-Check"-dialog for better shooting and less reading (I know what you want)
    - Also: This is the first version published to Foundry VTTs system repository (Partyyy :tada:)
    - Minor CSS changes have been made

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-Disclaimer: All of this was build on the Boilerplate System of FoundryVTT which was distributed for mingeling around in it. Thanks Atropos for this! (https://gitlab.com/foundrynet/worldbuilding)
+Disclaimer: This is the first, unofficial Foundry VTT system for Eclipse Phase 2nd Edition. All official logos, icons or other trademarks are intellectual property of Posthuman Studios, LLC (https://eclipsephase.com) and used with their permission. 
 
-This system is an eclipse phase pen & paper system for the Foundry VTT software. It's build by a total newbie so do not expect much, but if I did not write it otherwise I'll maintain the code and try to improve it over time. Feel free to get your own clone to try to make it even better (if this is something I need to give you access right to just write me). 
+Rules and non-registered trademarks are published under (CC) BY-NC-SA (https://creativecommons.org/licenses/by-nc-sa/3.0/). This is also true for any additions made or code written by the developer (me)
 
-If you want to use it for your own campaigns: Go for it! And please have fun.
+All of this was build on the Boilerplate System of FoundryVTT which was distributed for mingling around in it. Thanks, Atropos for this! (https://gitlab.com/foundrynet/worldbuilding)
+
+It's build by a total newbie so do not expect much, but if I did not write it otherwise I'll maintain the code and try to improve it over time. Feel free to get your own fork (hue hue hue) to try to make it even better.
+
+If you want to use it for your own campaigns: Go for it! Please have fun.
--- module/dice.js
+++ module/dice.js
@@ -1,20 +1,66 @@
+//General & Special Task Checks
 export async function TaskCheck({
+    //General
+    actorData = "",
     skillName = "",
     specName = "",
-    rollType = "",
-    skillValue = 0,
-    actorData = "",
-    globalMod = 0,
-    infectionMod = 0,
-    rollModeSelection = null,
-    activeRollTarget = "",
     spec = false,
+    skillValue = null,
     askForOptions = false,
     optionsSettings = null,
-    rollFormula = "1d100"
+    //Roll
+    rollType = "",
+    rollModeSelection = null,
+    activeRollTarget = "",
+    globalMod = null,
+    rollFormula = "1d100",
+    //Psi
+    infectionMod = null,
+    aspectPushes = null,
+    aspectBase = null,
+    //Guns
+    aim = "",
+    size = "",
+    range = "",
+    coverDefender = "",
+    coverAttacker = false,
+    visualImpairment = "",
+    prone = false,
+    firingMode = "",
+    smartlink = false,
+    running = false,
+    superiorPosition = false,
+    calledShot = false,
+    inMelee = false
     } = {}) {
 
-    if (askForOptions != optionsSettings && skillName === "psi") {
+    //Guns check dialog
+    if (askForOptions != optionsSettings && skillName === "guns") {
+        let checkOptions = await GetGunsTaskOptions(rollType);
+
+        if (checkOptions.cancelled) {
+            return;
+        }
+
+        globalMod = checkOptions.globalMod;
+        activeRollTarget = checkOptions.activeRollMode;
+        aim = checkOptions.aim;
+        size = checkOptions.size;
+        range = checkOptions.range;
+        coverDefender = checkOptions.coverDefender;
+        coverAttacker = checkOptions.coverAttacker;
+        visualImpairment = checkOptions.visualImpairment;
+        prone = checkOptions.prone;
+        firingMode = checkOptions.firingMode;
+        smartlink = checkOptions.smartlink;
+        running = checkOptions.running
+        superiorPosition = checkOptions.superiorPosition;
+        calledShot = checkOptions.calledShot;
+        inMelee = checkOptions.inMelee;
+    }
+
+    //Psi check dialog
+    else if (askForOptions != optionsSettings && skillName === "psi") {
         let checkOptions = await GetPsiTaskOptions(rollType);
 
         if (checkOptions.cancelled) {
@@ -23,9 +69,11 @@ export async function TaskCheck({
 
         globalMod = checkOptions.globalMod;
         activeRollTarget = checkOptions.activeRollMode;
-        infectionMod = checkOptions.pushes ? checkOptions.aspects*checkOptions.pushes : checkOptions.aspects;
+        aspectPushes = checkOptions.pushes
+        aspectBase = checkOptions.aspects
     }
 
+    //Default skill check dialog
     else if (askForOptions != optionsSettings) {
         let checkOptions = await GetTaskOptions(rollType);
 
@@ -37,15 +85,153 @@ export async function TaskCheck({
         activeRollTarget = checkOptions.activeRollMode;
     }
 
-    spec = specName ? "(" + specName + ")" : "";
-    let situationalPlus = globalMod>0 ? "+" : "";
+    let gunsMod = 0;
+    let gunAnnounce = "";
+    let gunModTitle = "";
+    console.log("Smartlink: " + smartlink);
+    console.log("Running: " + running);
+    console.log("Superior Position: " + superiorPosition);
+    console.log("Called Shot: " + calledShot);
+    console.log("In Melee: " + inMelee);
+    console.log("Covered Attacker: " + coverAttacker);
+    //Guns roll modifications
+    if (!smartlink) {
+        gunsMod -= 10;
+        gunAnnounce += "<br>No Smartgun (-10)";
+    }
+    if (running) {
+        gunsMod -= 20;
+        gunAnnounce += "<br>Running (-20)";
+    }
+    if (superiorPosition) {
+        gunsMod += 20;
+        gunAnnounce += "<br>Superior Position (+20)";
+    }
+    if (calledShot) {
+        gunsMod -= 10;
+        gunAnnounce += "<br>Called Shot (-10)";
+    }
+    if (inMelee) {
+        gunsMod -= 20;
+        gunAnnounce += "<br>Stuck in Melee (-10)";
+    }
+
+    if (coverAttacker) {
+        gunsMod -= 10;
+        gunAnnounce += "<br>In Cover (-10)";
+    }
+
+    if (aim === "quick") {
+        gunsMod += 10;
+        gunAnnounce += "<br>Quick Aim (+10)";
+    }
+    else if (aim === "long") {
+        gunsMod += 30;
+        gunAnnounce += "<br>Long Aim (+30)";
+    }
+
+    if (size === "xs") {
+        gunsMod -= 30;
+        gunAnnounce += "<br>Very Small Target (-30)";
+    }
+    else if (size === "s") {
+        gunsMod -= 10;
+        gunAnnounce += "<br>Small Target (-10)";
+    }
+    else if (size === "l") {
+        gunsMod += 10;
+        gunAnnounce += "<br>Large Target (+10)";
+    }
+    else if (size === "xl") {
+        gunsMod += 30;
+        gunAnnounce += "<br>Very Large Target (+30)";
+    }
+
+    if (range === "range" && prone) {
+        gunsMod -= 20;
+        gunAnnounce += "<br>Prone at Range (-20)";
+    }
+    else if (range === "beyond" && prone) {
+        gunsMod -= 30;
+        gunAnnounce += "<br>Prone Beyond Range (-30)";
+    }
+    else if (range === "beyond+" && prone) {
+        gunsMod -= 40;
+        gunAnnounce += "<br>Prone Far Beyond Range (-40)";
+    }
+    else if (range === "range") {
+        gunsMod -= 10;
+        gunAnnounce += "<br>At Range (-10)";
+    }
+    else if (range === "beyond") {
+        gunsMod -= 20;
+        gunAnnounce += "<br>Beyond Range (-20)";
+    }
+    else if (range === "beyond+") {
+        gunsMod -= 30;
+        gunAnnounce += "<br>Far Beyond Range (-30)";
+    }
+
+
+    if (coverDefender === "minor") {
+        gunsMod -= 10;
+        gunAnnounce += "<br>Target in minor Cover (-10)";
+    }
+    else if (coverDefender === "moderate") {
+        gunsMod -= 20;
+        gunAnnounce += "<br>Target in moderate Cover (-20)";
+    }
+    else if (coverDefender === "major") {
+        gunsMod -= 30;
+        gunAnnounce += "<br>Target in major Cover (-30)";
+    }
+
+    if (visualImpairment === "minor") {
+        gunsMod -= 10;
+        gunAnnounce += "<br>Minor Visual Impairment (-10)";
+    }
+    else if (visualImpairment === "major") {
+        gunsMod -= 20;
+        gunAnnounce += "<br>Moderate Visual Impaired (-20)";
+    }
+    else if (visualImpairment === "blind") {
+        gunsMod -= 30;
+        gunAnnounce += "<br>Blind (-30)";
+    }
+
+    if (firingMode === "burst") {
+        gunsMod += 10;
+        gunAnnounce += "<br>Wide Burst (+10)";
+    }
+    else if (firingMode === "fullAuto") {
+        gunsMod += 30;
+        gunAnnounce += "<br>Wide Full Auto (+30)";
+    }
+    else if (firingMode === "indirect") {
+        gunsMod -= 20;
+        gunAnnounce += "<br>Indirect (-20)";
+    }
+
+    gunModTitle = gunAnnounce ? "<p/><u>Shooting Modifiers</u>" : "";
+    console.log("The Guns Mod: " + gunsMod);
+    console.log("The Gun Announce:" + gunAnnounce);
+
+    //General roll modifications
     let woundMod = Number(actorData.mods.woundMod) + Number(actorData.mods.traumaMod);
     let rollMod = Number(globalMod) - woundMod;
+    let modSkillValue = Number(skillValue) + Number(globalMod) + Number(gunsMod);
+
+    //Chat message variables
+    spec = specName ? "(" + specName + ")" : "";
+    let situationalPlus = globalMod>0 ? "+" : "";
     let modAnnounce = rollMod ? "<u>Applied Mods:</u> <br>" : "";
     let woundAnnounce = woundMod ? "Wound/Trauma:<strong> -" + woundMod + "<br>" : "";
     let globalAnnounce = globalMod ? "</strong>Situational:<strong>"+ situationalPlus + globalMod : "";
-    let modSkillValue = Number(skillValue) + Number(globalMod);
+
+    //The dice roll
     let roll = new Roll(rollFormula).roll();
+
+    //Success check
     let rollCheck = roll.total;
     let success = rollCheck <= modSkillValue;
     let critical = rollCheck === 11 || rollCheck === 22 || rollCheck === 33 || rollCheck === 44 || rollCheck === 55 || rollCheck === 66 || rollCheck === 77 || rollCheck === 88;
@@ -54,26 +240,8 @@ export async function TaskCheck({
     let doubleSuperior = rollCheck >= 66;
     let superior = rollCheck >= 33;
     let successMessage = "";
-    let rollVisibility = "";
-    let infectionAddition = "";
 
-    if (skillName === "psi") {
-        console.log("Infection Mod Before:" + infectionMod)
-        infectionMod += Number(actorData.psiStrain.infection)
-        infectionAddition = "<p/>Infection raises to<p/>" + infectionMod
-        console.log("Infection Mod After:" + infectionMod)
-    }
-
-    if (activeRollTarget === "" || activeRollTarget === "public") {
-        rollModeSelection = CONST.DICE_ROLL_MODES.PUBLIC
-    } else if (activeRollTarget === "private") {
-        rollModeSelection = CONST.DICE_ROLL_MODES.PRIVATE
-        rollVisibility = "<p/><h5 style='font-weight: normal; margin: 0;'>Private Roll <i class=\"fas fa-eye-slash\"></i></h5><p/>"
-    } else if (activeRollTarget === "blind") {
-        rollModeSelection = CONST.DICE_ROLL_MODES.BLIND
-        rollVisibility = "<p/><h5 style='font-weight: normal; margin: 0;'>Blind GM Roll <i class=\"fas fa-low-vision\"></i></h5><p/>"
-    }
-
+    //Success messages
     if (autofail) {
         successMessage = "<span class='fail'>Supreme Fail!</span> <p>";
     } else if (autoSuccess) {
@@ -104,15 +272,42 @@ export async function TaskCheck({
         successMessage = "<span class='fail'>Fail!</span> <p>";
     }
 
+    //Visibility toggler
+    let rollVisibility = "";
+    if (activeRollTarget === "" || activeRollTarget === "public") {
+        rollModeSelection = CONST.DICE_ROLL_MODES.PUBLIC
+    } else if (activeRollTarget === "private") {
+        rollModeSelection = CONST.DICE_ROLL_MODES.PRIVATE
+        rollVisibility = "<p/><h5 style='font-weight: normal; margin: 0;'>Private Roll <i class=\"fas fa-eye-slash\"></i></h5><p/>"
+    } else if (activeRollTarget === "blind") {
+        rollModeSelection = CONST.DICE_ROLL_MODES.BLIND
+        rollVisibility = "<p/><h5 style='font-weight: normal; margin: 0;'>Blind GM Roll <i class=\"fas fa-low-vision\"></i></h5><p/>"
+    }
 
-    let label = successMessage + rollVisibility + "Rolled <strong>" + skillName + spec + "</strong> check <br> against <strong>" + modSkillValue + "</strong><p> <h5 style='font-weight: normal; margin: 0;'>" + modAnnounce + woundAnnounce + globalAnnounce + infectionAddition + "</strong></h5>";
+    //Infection (only relevant for psi checks)
+    let infectionAddition = "";
+    console.log("Aspect Pushes As Per Dialog: " + aspectPushes)
+    if (success && doubleSuperior) {
+        aspectPushes -= 4
+    }
+    else if (success && superior) {
+        aspectPushes -= 2
+    }
+    infectionMod = aspectPushes>0 ? aspectBase*aspectPushes : aspectBase;
+    if (skillName === "psi") {
+        infectionMod += Number(actorData.psiStrain.infection)
+        infectionAddition = "<p/>Infection raises to:" + infectionMod
+    }
+
+    //Chat message constructor
+    let label = successMessage + rollVisibility + "Rolled <strong>" + skillName + spec + "</strong> check <br> against <strong>" + modSkillValue + "</strong><p> <h5 style='font-weight: normal; margin: 0;'>" + modAnnounce + woundAnnounce + globalAnnounce + infectionAddition + gunModTitle + gunAnnounce + "</strong></h5>";
     roll.toMessage({
         speaker: ChatMessage.getSpeaker({actor: this.actor}),
         flavor: label,
         rollMode: rollModeSelection
     });
 
-
+    //Skill check dialog constructor
     async function GetTaskOptions(rollType) {
         const template = "systems/eclipsephase/templates/chat/skill-test-dialog.html";
         const html = await renderTemplate(template, {});
@@ -139,6 +334,7 @@ export async function TaskCheck({
         });
     }
 
+    //Psi check dialog constructor
     async function GetPsiTaskOptions(rollType) {
         const template = "systems/eclipsephase/templates/chat/psi-test-dialog.html";
         const html = await renderTemplate(template, {});
@@ -165,9 +361,37 @@ export async function TaskCheck({
         });
     }
 
+    //Guns check dialog constructor
+    async function GetGunsTaskOptions(rollType) {
+        const template = "systems/eclipsephase/templates/chat/gun-test-dialog.html";
+        const html = await renderTemplate(template, {});
+
+        return new Promise(resolve => {
+            const data = {
+                title: "Guns Check",
+                content: html,
+                buttons: {
+                    cancel: {
+                        label: "Cancel",
+                        callback: html => resolve ({cancelled: true})
+                    },
+                    normal: {
+                        label: "Roll!",
+                        callback: html => resolve(_proGunsTaskCheckOptions(html[0].querySelector("form")))
+                    }
+                },
+                default: "normal",
+                close: () => resolve ({cancelled: true})
+            };
+
+            new Dialog(data, null).render(true);
+        });
+    }
+
+    //Psi check results
     function _proPsiTaskCheckOptions(form) {
         return {
-            globalMod: parseInt(form.GlobalMod.value),
+            globalMod: form.GlobalMod.value ? parseInt(form.GlobalMod.value) : 0,
             aspects: parseInt(form.AspectNumber.value),
             pushes: parseInt(form.PushesNumber.value)*2,
             activeRollMode: form.RollMode.value
@@ -175,16 +399,39 @@ export async function TaskCheck({
 
     }
 
+    //Guns skill check results
+    function _proGunsTaskCheckOptions(form) {
+        return {
+            globalMod: form.GlobalMod.value ? parseInt(form.GlobalMod.value) : 0,
+            activeRollMode: form.RollMode.value,
+            aim: form.Aim.value,
+            size: form.Size.value,
+            range: form.Range.value,
+            coverDefender: form.TargetCover.value,
+            coverAttacker: form.AttackerCover.checked,
+            visualImpairment: form.VisualImpair.value,
+            prone: form.DefenderProne.checked,
+            firingMode: form.FiringMode.value,
+            smartlink: form.Smartlink.checked,
+            running: form.Running.checked,
+            superiorPosition: form.SupPosition.checked,
+            calledShot: form.CalledShot.checked,
+            inMelee: form.Melee.checked
+        }
+
+    }
+
+    //General skill check results
     function _proTaskCheckOptions(form) {
         return {
-            globalMod: parseInt(form.GlobalMod.value),
+            globalMod: form.GlobalMod.value ? parseInt(form.GlobalMod.value) : 0,
             activeRollMode: form.RollMode.value
         }
 
     }
 }
 
-
+//Weapon damage rolls
 export async function DamageRoll({
         weaponName = "",
         weaponDamage = "",
