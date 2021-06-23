429fd5695711f028cfd88ed8eceb8a1134198cdb
Kris Pearson <kris@hydrow.com>
2021-06-22 20:12:07 -0400

    new compendia

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,65 @@
-const gulp = require('gulp');
-const prefix = require('gulp-autoprefixer');
-const sourcemaps = require('gulp-sourcemaps');
-const sass = require('gulp-sass');
+const gulp = require("gulp");
+const through2 = require("through2");
+const yaml = require("js-yaml");
+const Datastore = require("nedb");
+const cb = require("cb");
+const mergeStream = require("merge-stream");
+const clean = require("gulp-clean");
+const zip = require("gulp-zip");
+const fs = require("fs");
+const path = require("path");
+
+const MODULE = JSON.parse(fs.readFileSync("template.json"));
+const STATIC_FILES = ["template.json", "src/assets/**/*"];
+const PACK_SRC = "src/packs";
+const BUILD_DIR = "build";
+const DIST_DIR = "dist";
 
 /* ----------------------------------------- */
-/*  Compile Sass
+/*  Compile Compendia
 /* ----------------------------------------- */
 
-// Small error handler helper function.
-function handleError(err) {
-  console.log(err.toString());
-  this.emit('end');
+function compilePacks() {
+  // determine the source folders to process
+  const folders = fs.readdirSync(PACK_SRC).filter((file) => {
+    return fs.statSync(path.join(PACK_SRC, file)).isDirectory();
+  });
+
+  // process each folder into a compendium db
+  const packs = folders.map((folder) => {
+    const db = new Datastore({ filename: path.resolve(__dirname, BUILD_DIR, "packs", `${folder}.db`), autoload: true });
+    return gulp.src(path.join(PACK_SRC, folder, "/**/*.yml")).pipe(
+      through2.obj((file, enc, cb) => {
+        let json = yaml.loadAll(file.contents.toString());
+        db.insert(json);
+        cb(null, file);
+      })
+    );
+  });
+  return mergeStream.call(null, packs);
+}
+
+/* ----------------------------------------- */
+/*  Copy static files
+/* ----------------------------------------- */
+
+function copyFiles() {
+  return gulp
+    .src(STATIC_FILES, {
+      base: "src",
+    })
+    .pipe(gulp.dest(BUILD_DIR));
+}
+
+/* ----------------------------------------- */
+/*  Create distribution archive
+/* ----------------------------------------- */
+
+function createZip() {
+  return gulp
+    .src(`${BUILD_DIR}/**/*`)
+    .pipe(zip(`foundryvtt-${MODULE.name}-v${MODULE.version}.zip`))
+    .pipe(gulp.dest(DIST_DIR));
 }
 
 const SYSTEM_SCSS = ["scss/**/*.scss"];
@@ -32,10 +81,15 @@ function compileScss() {
 const css = gulp.series(compileScss);
 
 /* ----------------------------------------- */
-/*  Watch Updates
+/*  Other Functions
 /* ----------------------------------------- */
 
+function cleanBuild() {
+  return gulp.src(`${BUILD_DIR}`, { allowEmpty: true }, { read: false }).pipe(clean());
+}
+
 function watchUpdates() {
+  gulp.watch("src/**/*", gulp.series(cleanBuild, copyFiles, compilePacks));
   gulp.watch(SYSTEM_SCSS, css);
 }
 
@@ -43,8 +97,10 @@ function watchUpdates() {
 /*  Export Tasks
 /* ----------------------------------------- */
 
-exports.default = gulp.series(
-  compileScss,
-  watchUpdates
-);
+exports.clean = gulp.series(cleanBuild);
+exports.compile = gulp.series(compilePacks);
+exports.copy = gulp.series(copyFiles);
+exports.build = gulp.series(cleanBuild, copyFiles, compilePacks);
+exports.dist = gulp.series(createZip);
 exports.css = css;
+exports.default = gulp.series(cleanBuild, copyFiles, compilePacks, watchUpdates);
