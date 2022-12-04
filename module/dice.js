9dfe8a42e7f7f150924ae1d3c29e5ac6c8cf1fcd
Diemen <diemen@me.com>
2022-12-04 18:34:48 +0100

    - Corrected a bug that did not show the damage roll of a weapon in case of the roll being private.

--- module/dice.js
+++ module/dice.js
@@ -1455,18 +1455,12 @@ export async function TaskCheck({
                     let roll = await new Roll(rollFormula).evaluate({async: true});
                     let label = "Rolls damage with <br> <strong>" + weaponName + "</strong>";
 
-                    if (rollModeSelection === "gmroll"){
-                        ChatMessage.create({
-                            content: label,
-                            whisper: ChatMessage.getWhisperRecipients("GM")
-                        });
-                    }
-                    else {
-                        roll.toMessage({
-                            speaker: ChatMessage.getSpeaker({ actor: this.actor }),
-                            flavor: label
-                        });
-                    }
+                    roll.toMessage({
+                        speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+                        flavor: label
+                    },{
+                        rollMode: rollModeSelection
+                    });
 
                 }
                     
