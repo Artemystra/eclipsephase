5c50622c30bfaa67d02d1f97a9b4ca2f3d2754bc
Diemen <info@war-klar.de>
2023-05-30 17:03:47 +0200

    changed effect label to effect name

--- module/actor/EPactorSheet.js
+++ module/actor/EPactorSheet.js
@@ -746,7 +746,7 @@ export default class EPactorSheet extends ActorSheet {
 
       if (!restReset && restType === "long"){
         for (let effect of actor.effects){
-          if (effect.label === "Temp Ignore Trauma" || effect.label === "Temp Ignore Wound"){
+          if (effect.name === "Temp Ignore Trauma" || effect.name === "Temp Ignore Wound"){
             let effectID = effect._id;
             actor.deleteEmbeddedDocuments('ActiveEffect', [effectID]);
           }
@@ -754,7 +754,7 @@ export default class EPactorSheet extends ActorSheet {
       }
       else if (restReset) {
         for (let effect of actor.effects){
-          if (effect.label === "Temp Ignore Trauma" || effect.label === "Temp Ignore Wound"){
+          if (effect.name === "Temp Ignore Trauma" || effect.name === "Temp Ignore Wound"){
             let effectID = effect._id;
             actor.deleteEmbeddedDocuments('ActiveEffect', [effectID]);
           }
@@ -926,8 +926,15 @@ export default class EPactorSheet extends ActorSheet {
     //browses through ALL effects and identifies, whether they are bound to an item or not
     for (let effectScan of allEffects){
       if (effectScan.origin){
-        
+
+        //Replaces fromUuid for now. Need to look into this at a later
+        let Uuid = (effectScan.origin).split(".");
+        let parentItem = actor.items.get(Uuid[3]);
+
+        /*This generates a problem with legacy characters and do currently need a revamp
         let parentItem = await fromUuid(effectScan.origin)
+        */
+
         if (parentItem.system.boundTo){
 
           let effUpdateData=[];
