0feeaea15e14e49aa81a71bb80cfcddf81ce22e8
Artemystra <info@war-klar.de>
2024-09-30 11:01:14 +0200

    Merge from master

--- module/rolls/pools.js
+++ module/rolls/pools.js
@@ -16,7 +16,6 @@ export async function usePoolFromChat(data){
     const options = dataset.usepool
     const actor = game.actors.get(dataset.actorid)
     const rolledFrom = dataset.rolledfrom
-    console.log("This is my data", dataset)
     const recipientList = prepareRecipients(dataset.rollmode)
 
     let updateResult = await update(options, pool, null, actor)
